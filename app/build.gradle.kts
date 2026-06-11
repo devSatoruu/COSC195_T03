e609ff7ff7460446d6dc7e5fbe52e9871eab57c8
aaaaa <siddiqui8914@saskpolytech.ca>
2026-06-10 19:48:36 -0600

    added in another dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+
     alias(libs.plugins.kotlin.compose)
+
 }
 
 android {
@@ -57,4 +59,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("com.github.bumptech.glide:glide:5.0.5")
 }
